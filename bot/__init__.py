9968fce9be536bf7afab90fbe1eb2fca44048a41
Abhijith NT <abhijithnt64@gmail.com>
2021-01-06 12:01:53 +0530

    add logging and msgs

--- bot/__init__.py
+++ bot/__init__.py
@@ -4,10 +4,18 @@
 # (c) Abhijith N T ;-)
 # Thank you https://github.com/pyrogram/pyrogram :-)
 
-import os
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    handlers=[logging.FileHandler('log.txt'), logging.StreamHandler()],
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+LOGGER = logging.getLogger(__name__)
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 class msg():
     source = "\nsource:https://github.com/Abhijith-cloud/Telegram-MixDrop-Bot"
-    start = "\n<b>@thankappan369</b>"
+    start = "\n<b>This bot uploads telegram files to mixdrop.co @thankappan369</b>"
     error = "something is went wrong\n{error} \ncontact admin @thankappan369"
-    help = ""
+    help = "Usage: <b>Send any file or URL and the bot will upload it to mixdrop.co</b>"
