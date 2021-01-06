e5fabdd20fa9effead232ed30131d0d833433dda
Abhijith NT <abhijithnt64@gmail.com>
2021-01-06 14:59:17 +0530

    Updated

--- sample_config.py
+++ sample_config.py
@@ -2,3 +2,5 @@ class config():
     BOT_TOKEN = ''  # from @botfather
     APP_ID = ''     # from https://my.telegram.org/apps
     API_HASH = ''   # from https://my.telegram.org/apps
+    API_KEY = ''    # from https://mixdrop.co
+    API_EMAIL = ''  # from https://mixdrop.co
