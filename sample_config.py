24ade131a601e2a7938505c99dc2dfcc50bea7cb
Abhijith NT <abhijithnt64@gmail.com>
2021-01-08 22:25:36 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -1,6 +1,6 @@
 class config():
-    BOT_TOKEN = '1351501215:AAGv9yJbkpPnSRTNjF4uq66Y5C9uQBx5n3E'  # from @botfather
-    APP_ID = '1978879'     # from https://my.telegram.org/apps
-    API_HASH = '2cac64c780aa3997a6d1b6ec7e248ead'   # from https://my.telegram.org/apps
+    BOT_TOKEN = ''  # from @botfather
+    APP_ID = ''     # from https://my.telegram.org/apps
+    API_HASH = ''   # from https://my.telegram.org/apps
     API_KEY = ''    # from https://mixdrop.co
     API_EMAIL = ''  # from https://mixdrop.co
