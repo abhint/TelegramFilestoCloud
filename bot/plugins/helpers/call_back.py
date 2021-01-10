b58b3cd0b08834dc8f54eeec1940109080f12b6e
Abhijith NT <abhijithnt64@gmail.com>
2021-01-10 21:59:47 +0530

    My Mistake

--- bot/plugins/helpers/call_back.py
+++ bot/plugins/helpers/call_back.py
@@ -4,10 +4,4 @@ import pyrogram
 from bot.plugins.helpers.dowloader import fileDownload
 @Client.on_callback_query()
 async def server_selection(client, server):
-    # print(server)
-    server_name = server.data
-    if server_name == 'MixDrop':
-        # LOGGER.info(server)
-        await fileDownload(client, server)
-    if server_name == 'File.io':
-        await fileDownload(client, server)
+    await fileDownload(client, server)
