6e7763419c10a7a5ba8c7181ba2728b6658da123
Restyled.io <commits@restyled.io>
2021-01-15 05:27:59 +0000

    Restyled by autopep8

--- bot/plugins/cmd/cmd.py
+++ bot/plugins/cmd/cmd.py
@@ -4,8 +4,9 @@
 # Copyright ABHIJITH N T
 # Thank you https://github.com/pyrogram/pyrogram
 
-from pyrogram import Client,filters
+from pyrogram import Client, filters
 from bot import msg
+
 
 @Client.on_message(filters.command("start"))
 async def start(client, message):
@@ -13,13 +14,15 @@ async def start(client, message):
         chat_id=message.chat.id,
         text=f"Hey {message.from_user.first_name},{msg.start}{msg.source}",
         reply_to_message_id=message.message_id,
-        parse_mode = "html"
+        parse_mode="html"
     )
-@Client.on_message(filters.command(["help","h"]))
+
+
+@Client.on_message(filters.command(["help", "h"]))
 async def help(client, message):
     await client.send_message(
         chat_id=message.chat.id,
         text=f"Hey {message.from_user.first_name},{msg.help}{msg.source}",
         reply_to_message_id=message.message_id,
-        parse_mode = "html"
+        parse_mode="html"
     )
