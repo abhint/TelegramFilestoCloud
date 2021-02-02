feb19cfedc203c9c1803a8d79fe7107afdcfb6c2
Abhijith NT <abhijithnt64@gmail.com>
2021-02-02 16:40:04 +0530

    can you add an env var for private use ? (#7)

--- bot/plugins/cmd/cmd.py
+++ bot/plugins/cmd/cmd.py
@@ -4,22 +4,22 @@
 # Copyright ABHIJITH N T
 # Thank you https://github.com/pyrogram/pyrogram
 
-from pyrogram import Client,filters
-from bot import msg
+from bot.filetocloud import CloudBot, filters
+from bot import Msg
 
-@Client.on_message(filters.command("start"))
-async def start(client, message):
+@CloudBot.on_message(filters.command("start"))
+async def start_message(client, message):
     await client.send_message(
         chat_id=message.chat.id,
-        text=f"Hey {message.from_user.first_name},{msg.start}{msg.source}",
+        text=f"Hey {message.from_user.first_name},{Msg.start}{Msg.source}",
         reply_to_message_id=message.message_id,
-        parse_mode = "html"
+        parse_mode="html"
     )
-@Client.on_message(filters.command(["help","h"]))
-async def help(client, message):
+@CloudBot.on_message(filters.command(["help", "h"]))
+async def help_message(client, message):
     await client.send_message(
         chat_id=message.chat.id,
-        text=f"Hey {message.from_user.first_name},{msg.help}{msg.source}",
+        text=f"Hey {message.from_user.first_name},{Msg.help}{Msg.source}",
         reply_to_message_id=message.message_id,
-        parse_mode = "html"
+        parse_mode="html"
     )
--- bot/plugins/helpers/dowloader.py
+++ bot/plugins/helpers/dowloader.py
@@ -7,38 +7,44 @@
 from bot.plugins.upload_servers.fileio_upload import fileIO
 from bot.plugins.upload_servers.mixdrop_upload import mixFileup
 from bot.plugins.display import progress
-import os, time
+from bot import LOGGER
+import os
+import time
 from pyrogram.errors import FloodWait
+
 
 async def fileDownload(client, bot):
     file_path = ''
     now = time.time()
     upload_server = bot.data
-    userMsg = await client.edit_message_text(
+    user_message = await client.edit_message_text(
         chat_id=bot.from_user.id,
         message_id=bot.message.message_id,
         text="processing your request...please wait",
     )
-    user_progress = userMsg
+    user_progress = user_message
     try:
         file_path = await client.download_media(
-        message = userMsg.reply_to_message,
-        progress=progress,
-        progress_args=(
-            "Downloading...",
-            user_progress,
-            now
+            message=user_message.reply_to_message,
+            progress=progress,
+            progress_args=(
+                "Downloading...",
+                user_progress,
+                now
             )
         )
     except FloodWait as e:
+        LOGGER.info(f"{e}")
         print(time.sleep(e.x))
 
     if upload_server == "MixDrop":
-        await mixFileup(file_path,client, bot, now)
+        await mixFileup(file_path, client, bot, now)
     if upload_server == "File.io":
         await fileIO(file_path, client, bot, now)
 
     try:
         os.remove(file_path)
+        LOGGER.info(f"{file_path}")
     except OSError as error:
+        LOGGER.info(f"Error: {error.filename} - {error.strerror}.")
         print("Error: %s - %s." % (error.filename, error.strerror))
