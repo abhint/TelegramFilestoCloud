2302cc4b6d828067c6e8070e0dfb84f9bd4daf83
Abhijith N T <abhijithnt64@gmail.com>
2021-05-15 21:56:31 +0530

    Update call_back.py

--- bot/plugins/helpers/call_back.py
+++ bot/plugins/helpers/call_back.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
-# This is bot coded by AbhijithCloud and used for educational purposes only
-# https://github.com/AbhijithCloud
+# This is bot coded by Abhijith N T and used for educational purposes only
+# https://github.com/AbhijithNT
 # Copyright ABHIJITH N T
 # Thank you https://github.com/pyrogram/pyrogram
 from bot.filetocloud import CloudBot
