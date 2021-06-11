ed3bbfaa61e755603fb7d3ccd7537eb5c639b933
AbhijithNT <abhijithnt64@gmail.com>
2021-06-11 22:14:32 +0530

    🚀 Private & Public Use

--- sample_config.py
+++ sample_config.py
@@ -1,4 +1,7 @@
 class Config:
-    BOT_TOKEN = ''                                                  # from @botfather
-    APP_ID =                                                        # from https://my.telegram.org/apps
-    API_HASH = ''                                                   # from https://my.telegram.org/apps
+    BOT_USE = True
+    BOT_TOKEN = '1351501215:AAG8lVdSM3EZxnzGouODCTRHuOKM_3wbWpQ'        # from @botfather
+    APP_ID =  1978879           # from https://my.telegram.org/apps
+    API_HASH = '2cac64c780aa3997a6d1b6ec7e248ead'         # from https://my.telegram.org/apps
+    AUTH_USERS = []
+ 
