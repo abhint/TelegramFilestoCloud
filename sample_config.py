252f7f8be02585e3c2ef7987ac7a550280cda4c7
AbhijithNT <abhijithnt64@gmail.com>
2021-06-16 22:06:23 +0530

    :white_check_mark:

--- sample_config.py
+++ sample_config.py
@@ -1,5 +1,5 @@
 class Config:
-    BOT_USE = True
+    BOT_USE = False
     BOT_TOKEN = ''                         # from @botfather
     APP_ID =                               # from https://my.telegram.org/apps
     API_HASH = ''                          # from https://my.telegram.org/apps
