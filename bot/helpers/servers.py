17826748d699c5819c0c44245825d5a04e68b39a
AbhijithNT <abhijithnt64@gmail.com>
2021-12-04 22:45:57 +0530

    upload_handler()

--- bot/helpers/servers.py
+++ bot/helpers/servers.py
@@ -1,22 +1,75 @@
-from json import decoder
+from bot import LOGGER
 from ..filetocloud import CloudBot
-import aiohttp
 from .upload import server_upload
 from ..helpers import download_media
 from pyrogram.types import CallbackQuery
 
+logger = LOGGER(__name__)
 
-async def anonfiles(client: CloudBot, message: CallbackQuery):
+
+async def upload_handler(client: CloudBot, message: CallbackQuery, file_name: str, file_size: str, callback_data: str):
 
     try:
-        FILE_PATH = await download_media(client, "message")
+        FILE_PATH = await download_media(client, message)
     except Exception as e:
-        print(e)
-        
+        logger.error(f"{e}")
+        await client.edit_message_text(
+            chat_id=message.from_user.id,
+            message_id=message.message.message_id,
+            text=f"**File downloading error:** `{e}`",
+        )
         return
     try:
-        url = 'https://api.anonfiles.com/upload'
-        d = await server_upload(file="filetocloud_log.txt", url=url)
-        print(d)
+        if callback_data.startswith('transfersh'):
+            await client.edit_message_text(
+                chat_id=message.message.chat.id,
+                text="Temporarily down",
+                message_id=message.message.message_id,
+            )
+            return
+
+        elif callback_data.startswith('gofileio'):
+            url = 'https://store9.gofile.io/uploadFile'
+            response = await server_upload(file=FILE_PATH, url=url)
+            _URL = await gofileIO(response)
+
+        elif callback_data.startswith('fileio'):
+            url = 'https://file.io/'
+            response = await server_upload(file=FILE_PATH, url=url)
+            _URL = await fileIO(response)
+
+        elif callback_data.startswith('anonfiles'):
+            url = 'https://api.anonfiles.com/upload'
+            response = await server_upload(file=FILE_PATH, url=url)
+            _URL = await anonfiles(response)
+
+        await client.edit_message_text(
+            chat_id=message.message.chat.id,
+            text=(
+                f"File Name: `{file_name}`"
+                f"\nFile Size: `{file_size}`"
+                f'\nURL: `{_URL}`'
+            ),
+            message_id=message.message.message_id
+            # reply_markup=completedKeyboard(dl)
+        )
     except Exception as e:
-        print(f"ERROR: {e}")
+        logger.error(f'{e}')
+        await client.edit_message_text(
+            chat_id=message.from_user.id,
+            message_id=message.message.message_id,
+            text=f"**File uploading error:** `{e}`",
+        )
+        return
+
+
+async def anonfiles(response):
+    return response['data']['file']['url']['short']
+
+
+async def fileIO(response):
+    return response['link']
+
+
+async def gofileIO(response):
+    return response['data']['downloadPage']
