2f75524ff8aa89e5d3def43bf068c7faf357b0d4
AbhijithNT <abhijithnt64@gmail.com>
2021-12-04 17:32:13 +0530

    creating a .env file

--- bot/env.py
+++ bot/env.py
@@ -2,11 +2,14 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
+
 def get_env(name: str, terminal_action: bool = True) -> str:
     if name in os.environ:
         return os.environ[name]
     if terminal_action:
-        value_name = input(f'Enter your {name}: ')
-        return value_name
+        value = input(f'Enter your {name}: ')
+        with open('.env', 'a') as f:
+            f.write(f'{name}={value}\n')
+        return value
     else:
         return name
