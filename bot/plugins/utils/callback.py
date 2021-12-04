1ac754d90f30b918d7018604f3bc867cd7d2f896
AbhijithNT <abhijithnt64@gmail.com>
2021-12-04 20:04:04 +0530

    download media from the user

--- bot/plugins/utils/callback.py
+++ bot/plugins/utils/callback.py
@@ -1,8 +1,14 @@
+from logging import fatal
 from ...filetocloud import CloudBot
 from pyrogram.types import CallbackQuery
-
+from ...helpers import download_media
 
 
 @CloudBot.on_callback_query()
 async def selecting_server(client: CloudBot, message: CallbackQuery) -> None:
-    print(message)
+    callback_data = message.data
+    if callback_data.startswith('transfersh'):
+        f = await download_media(
+            client,
+            message
+        )
