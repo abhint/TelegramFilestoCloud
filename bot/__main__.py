831819b4b825ba959bd1cefdb9327c7ead5a182d
AbhijithNT <abhijithnt64@gmail.com>
2021-12-04 17:08:35 +0530

    run me

---
+++ bot/__main__.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# This is bot coded by AbhijithCloud and used for educational purposes only
+# https://github.com/AbhijithNT
+# Copyright ABHIJITH N T
+# Thank you https://github.com/pyrogram/pyrogram
+
+import pyrogram
+from bot.filetocloud import CloudBot
+import logging
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+LOGGER = logging.getLogger(__name__)
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
+if __name__ == "__main__":
+    CloudBot().run()
+
