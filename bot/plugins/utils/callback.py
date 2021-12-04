610e38b64ef01ac0cd2ef78fbdf749a3e76613cf
AbhijithNT <abhijithnt64@gmail.com>
2021-12-04 22:46:56 +0530

    conditions

--- bot/plugins/utils/callback.py
+++ bot/plugins/utils/callback.py
@@ -2,17 +2,19 @@ from logging import fatal
 from ...filetocloud import CloudBot
 from pyrogram.types import CallbackQuery
 from ...helpers import download_media
-from ...helpers.servers import anonfiles
+from ...helpers.servers import upload_handler
 
 
 @CloudBot.on_callback_query()
 async def selecting_server(client: CloudBot, message: CallbackQuery) -> None:
     callback_data = message.data
+    file_name, file_size = callback_data.split(
+        '|')[1], callback_data.split('|')[2]
     if callback_data.startswith('transfersh'):
-        await anonfiles(client, message)
+        await upload_handler(client, message, file_name, file_size, callback_data)
     elif callback_data.startswith('fileio'):
-        pass
+        await upload_handler(client, message, file_name, file_size, callback_data)
     elif callback_data.startswith('gofileio'):
-        pass
+        await upload_handler(client, message, file_name, file_size, callback_data)
     elif callback_data.startswith('anonymfiles'):
-        pass
+        await upload_handler(client, message, file_name, file_size, callback_data)
