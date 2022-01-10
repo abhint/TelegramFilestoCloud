3c2695b9036574356ae2d20bec3c33888c5f4ed3
AbhijithNT <abhijithnt64@gmail.com>
2022-01-10 21:44:44 +0530

    downloading progress removed (#33)
    Signed-off-by: AbhijithNT <abhijithnt64@gmail.com>

--- bot/helpers/file_download.py
+++ bot/helpers/file_download.py
@@ -4,6 +4,7 @@ from ..filetocloud import CloudBot
 from pyrogram.errors import RPCError
 from pyrogram.types import CallbackQuery
 from ..helpers.display import progress
+
 # from ..helpers import progress
 
 
@@ -12,23 +13,18 @@ logger = LOGGER(__name__)
 
 async def download_media(client: CloudBot, message: CallbackQuery, ) -> str:
     # client.download_media(message)
-    now = time.time()
     user_message = await client.edit_message_text(
         chat_id=message.from_user.id,
         message_id=message.message.message_id,
-        text="processing your request...please wait",
+        text="processing your request...",
     )
     try:
         media_id = message.message.reply_to_message
-        download_file_path = await client.download_media(media_id,
-                                                         progress=progress,
-                                                         progress_args=(
-                                                             "Downloading...",
-                                                             user_message,
-                                                             now
-                                                         )
-                                                         )
-        print(download_file_path)
+        download_file_path = await client.download_media(
+            media_id,
+            progress=progress,
+            progress_args=(user_message,)
+        )
         return download_file_path
     except RPCError as e:
         logger.error(e)
