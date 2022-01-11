3c830373299a4ce16131aee0029b4e1bedaab9ca
AbhijithNT <abhijithnt64@gmail.com>
2022-01-11 12:19:53 +0530

    unused arguments removed (#40)
    Signed-off-by: AbhijithNT <abhijithnt64@gmail.com>

--- bot/plugins/utils/callback.py
+++ bot/plugins/utils/callback.py
@@ -8,13 +8,11 @@ from ...helpers.servers import upload_handler
 @CloudBot.on_callback_query()
 async def selecting_server(client: CloudBot, message: CallbackQuery) -> None:
     callback_data = message.data
-    file_name, file_size = callback_data.split(
-        '|')[1], callback_data.split('|')[2]
     if callback_data.startswith('transfersh'):
-        await upload_handler(client, message, file_name, file_size, callback_data)
+        await upload_handler(client, message, callback_data)
     elif callback_data.startswith('fileio'):
-        await upload_handler(client, message, file_name, file_size, callback_data)
+        await upload_handler(client, message, callback_data)
     elif callback_data.startswith('gofileio'):
-        await upload_handler(client, message, file_name, file_size, callback_data)
+        await upload_handler(client, message, callback_data)
     elif callback_data.startswith('anonymfiles'):
-        await upload_handler(client, message, file_name, file_size, callback_data)
+        await upload_handler(client, message, callback_data)
