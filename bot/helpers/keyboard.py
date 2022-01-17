0d01eba65e764a9e0f05d162f93dee44a24d8011
AbhijithNT <abhijithnt64@gmail.com>
2022-01-17 21:24:18 +0530

    username changed
    Signed-off-by: AbhijithNT <abhijithnt64@gmail.com>

--- bot/helpers/keyboard.py
+++ bot/helpers/keyboard.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # This is bot coded by Abhijith N T and used for educational purposes only
-# https://github.com/AbhijithNT
+# https://github.com/abhint
 # Copyright ABHIJITH N T
 # Thank you https://github.com/pyrogram/pyrogram
 
