f8f69befbf338b491c2ccbf2f9e4063fe9d35a4b
AbhijithNT <abhijithnt64@gmail.com>
2022-01-18 10:57:35 +0530

    parse_mode html to Markdown
    Signed-off-by: AbhijithNT <abhijithnt64@gmail.com>

--- bot/plugins/commands/help.py
+++ bot/plugins/commands/help.py
@@ -8,5 +8,4 @@ async def help_message(client, message):
         chat_id=message.chat.id,
         text=f"Hey {message.from_user.first_name},{HELP}{SOURCE}",
         reply_to_message_id=message.message_id,
-        parse_mode="html"
     )
