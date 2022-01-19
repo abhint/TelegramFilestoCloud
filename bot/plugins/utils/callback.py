c1fbe30d711ded0511596df3fbf633e48cde122d
AbhijithNT <abhijithnt64@gmail.com>
2022-01-19 21:25:07 +0530

    no condition is required
    Signed-off-by: AbhijithNT <abhijithnt64@gmail.com>

--- bot/plugins/utils/callback.py
+++ bot/plugins/utils/callback.py
@@ -6,9 +6,4 @@ from ...helpers.servers import upload_handler
 @CloudBot.on_callback_query()
 async def selecting_server(client: CloudBot, message: CallbackQuery) -> None:
     callback_data = message.data
-    if callback_data.startswith('fileio'):
-        await upload_handler(client, message, callback_data)
-    elif callback_data.startswith('gofileio'):
-        await upload_handler(client, message, callback_data)
-    elif callback_data.startswith('anonymfiles'):
-        await upload_handler(client, message, callback_data)
+    await upload_handler(client, message, callback_data)
